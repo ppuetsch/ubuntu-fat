6c1a1a057f9d083868de6a17967a1672109e6fd3
Philip Pütsch <philip.puetsch@d-fine.de>
2020-07-13 19:42:20 +0200

    adds a name to the started container

--- ubuntu.bat
+++ ubuntu.bat
@@ -1,1 +1,1 @@
-docker run -ti -v %cd%:/hostDirectory -v /var/run/docker.sock:/var/run/docker.sock -v ubuntu-root:/root ppuetsch/ubuntu-fat:latest
+docker run -ti --name ubuntu-fat -v %cd%:/hostDirectory -v /var/run/docker.sock:/var/run/docker.sock -v ubuntu-root:/root ppuetsch/ubuntu-fat:latest
