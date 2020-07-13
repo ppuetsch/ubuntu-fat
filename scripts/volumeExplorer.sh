d1c1d35f7edb3ad17fd6eff27de1f90bc1841cda
Philip Pütsch <philip.puetsch@d-fine.de>
2020-07-13 20:06:47 +0200

    small improvements

--- scripts/volumeExplorer.sh
+++ scripts/volumeExplorer.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-docker run -ti --name ubuntu-fat-volume-explorer -v ubuntu-root:/root -v /var/run/docker.sock:/var/run/docker.sock $(docker volume list | awk '{printf "%s", " -v"$2":/docker-volumes/"$2}') ppuetsch/ubuntu-fat:latest
+HOST_VOLUME_MOUNT="-v $(docker inspect $HOSTNAME | grep ":/hostDirectory" | tr ',"' ' ')"
+ALL_VOLUME_MOUNTS=$(docker volume list | awk '{printf "%s", " -v"$2":/docker-volumes/"$2}')
+DOCKER_SOCKET_MOUNT="-v /var/run/docker.sock:/var/run/docker.sock"
+ROOT_VOLUME_MOUNT="-v ubuntu-root:/root"
+
+docker run -ti $ROOT_VOLUME_MOUNT $HOST_VOLUME_MOUNT $DOCKER_SOCKET_MOUNT $ALL_VOLUME_MOUNTS ppuetsch/ubuntu-fat:latest
--- scripts/volumeSaver.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -ti --name ubuntu-fat-volume-explorer -v ubuntu-root:/root -v /var/run/docker.sock:/var/run/docker.sock $(docker volume list | awk '{printf "%s", " -v"$2":/docker-volumes/"$2}') ppuetsch/ubuntu-fat:latest
